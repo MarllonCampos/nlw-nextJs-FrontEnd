9aef3a42182349f9330aa5f99827d111e13be5de
Marllon Campos <marllondcsp@gmail.com>
2021-02-24 23:30:19 -0300

    Criado o componente que mostra a foto, o nome e o level do usuário, junto com seu módulo css

---
+++ src/components/Profile.tsx
@@ -0,0 +1,15 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/marlloncampos.png" alt="Marllon campos" />
+            <div>
+                <strong>Marllon Campos</strong>
+                <p>
+                    <img src="icons/level.svg" alt="icon-level-up"/>    
+                    Level 1</p>
+            </div>
+        </div>
+    );
+}
